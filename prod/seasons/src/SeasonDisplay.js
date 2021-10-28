688db4eecb9b93480906e8ee8f61712791c82817
Koscee <koscee.anason@yahoo.com>
2021-10-28 14:34:06 +0800

    Implement logic for determining season.

--- prod/seasons/src/SeasonDisplay.js
+++ prod/seasons/src/SeasonDisplay.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+/* Functional Component */
 const SeasonDisplay = (props) => {
-  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  console.log(season);
+
   return <div>Seasons Display</div>;
 };
 
