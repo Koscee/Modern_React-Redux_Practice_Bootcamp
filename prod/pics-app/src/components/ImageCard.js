c20a892445298f6da925167f7313a4059ca9ebdf
Koscee <koscee.anason@yahoo.com>
2021-11-03 15:21:24 +0800

    Callbacks on image load.

--- prod/pics-app/src/components/ImageCard.js
+++ prod/pics-app/src/components/ImageCard.js
@@ -10,10 +10,16 @@ class ImageCard extends React.Component {
   componentDidMount() {
     /**
      * the clientHeight initially will be 0 in the broswer console
-     * because once this component mounts, the image has not loaded yet
+     * because once this component mounts, the image has not loaded yet.
+     * A load event listener is added so when the image loads,
+     * it calls the callback function
      */
-    console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
 
   render() {
     const { description, urls } = this.props.image;
