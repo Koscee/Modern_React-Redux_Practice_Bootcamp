4ce137a1ec61bfc7908b533397be45647dae7b78
Koscee <koscee.anason@yahoo.com>
2021-11-03 14:59:44 +0800

    Access image height using React.createRef().

--- prod/pics-app/src/components/ImageCard.js
+++ prod/pics-app/src/components/ImageCard.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    /**
+     * the clientHeight initially will be 0 in the broswer console
+     * because once this component mounts, the image has not loaded yet
+     */
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
