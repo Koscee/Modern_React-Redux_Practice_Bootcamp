0bba460c9910060dcc6cd409ce62f15b013a4c9a
Koscee <koscee.anason@yahoo.com>
2021-11-11 16:55:25 +0800

    Fetch videos data from youtube api on submitimg search term.

--- prod/videos/src/components/App.js
+++ prod/videos/src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
 
 export default class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get("/search", {
+      params: { q: term },
+    });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
