0886c07f08e5a987de3deebbd731b1c7597fb75c
Koscee <koscee.anason@yahoo.com>
2021-11-17 16:26:37 +0800

    Add onVideoSelect callback to pass selected video from VideoItem (child) back to App component (parent).

--- prod/videos/src/components/VideoList.js
+++ prod/videos/src/components/VideoList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
     // TODO: add the key prop
-    return <VideoItem video={video} />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
